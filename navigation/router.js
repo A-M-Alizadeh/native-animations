8dba6bde8017b57125838b7013b65c6a1af92d0d
GrayDev <agraydev@gmail.com>
2019-12-04 11:34:01 +0330

    initial commit

---
+++ navigation/router.js
@@ -0,0 +1,10 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const AppNavigator = createStackNavigator({
+    Home: {
+      screen: HomeScreen,
+    },
+  });
+  
+  export default createAppContainer(AppNavigator);
